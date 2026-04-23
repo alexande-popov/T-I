e7a44bf0ea95f36ee1cabc7403b35f731cdf3110
Alexander Popov <alexande-popov@yandex.ru>
2026-04-23 23:36:20 +0300

    Add first client

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from t-i!")
+import os
+from dotenv import load_dotenv
+from t_tech.invest import Client
 
+
+load_dotenv()
+TOKEN = os.getenv("TINKOFF_TOKEN_ONLY_READ")
+
+def main():
+    # Создаем клиента с токеном
+    with Client(TOKEN) as client:
+        # Пример 1: Получить информацию о счетах
+        accounts = client.users.get_accounts()
+        for account in accounts.accounts:
+            print(f"Счет: {account.id}, Статус: {account.status}")
 
 if __name__ == "__main__":
     main()
